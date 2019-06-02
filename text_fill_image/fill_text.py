7014fa41ceda51c71d5389787d40f65eb6290399
AlistairSymonds <alistair.symonds@gmail.com>
2019-06-02 13:09:34 +1000

    Fills in image with randomly rotated text

--- text_fill_image/fill_text.py
+++ text_fill_image/fill_text.py
@@ -3,22 +3,15 @@ from PIL import ImageFont
 from PIL import Image
 from PIL import ImageDraw
 import text_fill_image.fill_text.random_fill as f
+from pathlib import Path
+from fontTools.ttLib import TTFont
 
-
-
-def fill(text: str, canvas, mask):
-    font = ImageFont.truetype("C:/Users/alist/OneDrive/Documents/Memes/cm-unicode-0.7.0/cmunrm.ttf", 600)
-
-
-
-    draw = ImageDraw.Draw(canvas)
-    draw.text((0, 0), text, (0, 0, 0), font=font)
-    draw = ImageDraw.Draw(canvas)
-
-
-    return canvas, mask
-
-
+def char_in_font(unicode_char, font):
+    for cmap in font['cmap'].tables:
+        if cmap.isUnicode():
+            if ord(unicode_char) in cmap.cmap:
+                return True
+    return False
 
 ap = argparse.ArgumentParser()
 ap.add_argument("--font_dir", "-fd", help="Paths(s) to font directories", nargs='+')
@@ -35,6 +28,25 @@ img = Image.new("RGBA", (w_px, h_px), (255, 255, 255, 0))
 mask = Image.new("L", (w_px, h_px),(255))
 text = "βΛΣ"
 
-img, mask = f.fill(text, img, mask)
-img.save("a_test.png")
-mask.save("a_mask.png")
+font_files = []
+for font_dir in args.font_dir:
+    for ff in Path(font_dir).glob("*.ttf"):
+        print(ff)
+        has_all_chars = True
+        for test_char in text:
+            test_font = TTFont(str(ff))
+            if(char_in_font(test_char, test_font)) == False:
+                has_all_chars= False
+            if has_all_chars:
+                font_files.append(ff)
+
+
+
+
+font = ImageFont.truetype("C:/Users/alist/OneDrive/Documents/Memes/cm-unicode-0.7.0/cmunrm.ttf", 1200)
+img = f.fill(text, (1000,2000), img, font_files, text_iters=50)
+img = f.fill(text, (500,1000), img, font_files, text_iters=50)
+img = f.fill(text, (250, 500), img, font_files, text_iters=150)
+img = f.fill(text, (100,250), img, font_files, text_iters=150)
+img = f.fill(text, (50,125), img, font_files, text_iters=150)
+img.save("fill_text_out.png")
--- text_fill_image/fill_text/random_fill.py
+++ text_fill_image/fill_text/random_fill.py
@@ -3,35 +3,68 @@ from PIL import ImageFont
 from PIL import Image
 from PIL import ImageDraw
 import random
+import numpy as np
+import matplotlib.pyplot as plt
+def fill(text: str, font_range: (int, int), canvas, font_files, attempts_per_text=5, text_iters=100):
 
-def fill(text: str, canvas, mask):
+    random.seed(42)
+    font_idx = 0
+    random.shuffle(font_files)
 
+    for i in range (0,text_iters):
+        #text = str(i)
+        print("== Pasting number: "+ str(i)+" ==")
+        font_size = random.randrange(font_range[0], font_range[1])
+        font = ImageFont.truetype(str(font_files[font_idx]), font_size)
+        font_idx = font_idx + 1
+        if font_idx % len(font_files) == 0:
+            font_idx = 0
+            random.shuffle(font_files)
 
+        text_mask = font.getmask(text)
+        text_w, text_h = text_mask.size
+        text_h = int(text_h*1.6)
+        temp_canvas = Image.new("RGBA", (text_w, text_h), (255, 255, 255, 0))
 
-    for i in range (0,10):
-        font_size = random.randrange(400, 600)
-        font = ImageFont.truetype("C:/Users/alist/OneDrive/Documents/Memes/cm-unicode-0.7.0/cmunrm.ttf", font_size)
-
-        temp_mask = font.getmask(text)
-        temp_canvas = Image.new("RGBA", (1000, 1000), (255, 255, 255, 0))
-        text_h, text_y = temp_mask.size
-        temp_mask_canvas = Image.new("RGBA", (text_h, text_y),(255,255,255,0))
-
-        temp_mask_draw = ImageDraw.Draw(temp_mask_canvas)
-        temp_mask_draw.rectangle([(0, 0), (text_h, text_y)], fill=0)
 
         temp_canvas_draw = ImageDraw.Draw(temp_canvas)
         temp_canvas_draw.text((0, 0), text, (0, 0, 0), font=font)
+        rotation_amount = random.randrange(0, 359)
+        temp_canvas = temp_canvas.rotate(rotation_amount, expand=True)
+        text_w, text_h = temp_canvas.size
+        canvas_w, canvas_h = canvas.size
+
+        #canvas_w = 4000
+        #canvas_h = 4000
+
+        paste_x_max = canvas_w - text_w
+        paste_y_max = canvas_h - text_h
+        for j in range(0,attempts_per_text):
+
+            if(paste_x_max < 0):
+                paste_x_max = 1
+            if ( paste_y_max < 0):
+                paste_y_max = 1
+            paste_x = random.randrange(0, paste_x_max)
+            paste_y = random.randrange(0, paste_y_max)
+            print("Gonna paste at: " + str(paste_x)+", "+str(paste_y))
+
+            canvas_np = np.array(canvas)
+            paste_dest = np.logical_not(canvas_np[paste_y:paste_y+text_h, paste_x:paste_x+text_w,0:3].astype(bool))
+            text_bool = np.logical_not(np.array(temp_canvas).astype(bool)[:,:,0:3])
+            intersect_points = np.logical_and(paste_dest, text_bool)
 
 
-        canvas_h, canvas_w = canvas.size
-        paste_x = random.randrange(0,canvas_w)
-        paste_y = random.randrange(0, canvas_h)
-
-        canvas.paste(temp_canvas,(paste_x,paste_y))
-
-
-
+            intersect_detected = np.any(np.any(np.any(intersect_points)))
+            if intersect_detected:
+                print("overlap, attempting again")
+            else:
+                canvas.paste(temp_canvas,(paste_x,paste_y),temp_canvas)
+                break
 
 
-    return canvas, mask
+
+
+
+
+    return canvas
